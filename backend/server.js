d2391e8055487ad233e5bfdba274107ba0bf1e59
VinayakGaikwad <vinaayakgaikwad@gmail.com>
2024-10-05 09:44:00 +0530

    Database Connectivity

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,26 @@
+// dependencies
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+// defined dependencies
+import connectDB from "./database/connectDB.js";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+dotenv.config();
+
+connectDB(process.env.MONGODB_URI);
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET, POST, PUT, PATCH, DELETE",
+    credentials: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
